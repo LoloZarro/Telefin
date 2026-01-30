6f0ea69c01080ddfbb3434d0ba6c4b1ef2259c8a
LoloZarro <lorenzo.bizzarro@gmx.ch>
2026-01-30 16:07:01 +0100

    Prevent duplicate playback start notifications within 5 seconds for the same item

---
+++ Telefin/Common/Models/LastPlayedItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Telefin.Common.Models
+{
+    public class LastPlayedItem()
+    {
+        public LastPlayedItem(Guid id, long timestamp) : this()
+        {
+            Id = id;
+            Timestamp = timestamp;
+        }
+
+        public Guid Id { get; set; }
+
+        public long Timestamp { get; set; }
+    }
+}
